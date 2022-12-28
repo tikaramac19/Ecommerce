03d7a08b3410ffa0706c00f2d16cb90cebd667d1
Tikaram Acharya <tikaramac19@gmail.com>
2022-12-28 15:38:22 +0545

    adding routing to the pages

--- todo-typescript/src/App.tsx
+++ todo-typescript/src/App.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
-import styled from 'styled-components';
-import './App.css';
-import Todo from './components/Todo';
+import React from "react";
+import styled from "styled-components";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Todo from "./components/Todo";
+import Login from "./Pages/Login/Login.pages";
+import Register from "./Pages/Register/Register.pages";
 function App() {
   return (
     <Wrapper>
-        <Title>Todo App Typescript</Title>
-        <Todo />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/todo" element={<Todo />} />
+          <Route path="/login" element={<Login message="Login Page" />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
     </Wrapper>
   );
 }
@@ -14,18 +22,12 @@ function App() {
 export default App;
 
 const Wrapper = styled.div`
-    margin : 0px;
-    padding : 0px;
-    height: 100vh;
-    width: 100%;
-    display : flex;
-    flex-direction : column;
-    align-items: center;
-    background-color : #757
-
-`
-const Title = styled.h1`
-    color: #fff;
-    font-weight: bold;
-    letter-spacing: 0.4rem;
-`
+  margin: 0px;
+  padding: 0px;
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #757;
+`;
