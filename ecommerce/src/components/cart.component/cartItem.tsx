1a58309668d88c3d9ae63e54e7bc3a52f4314fbe
Tikaram Acharya <tikaramac19@gmail.com>
2022-12-30 16:36:19 +0545

    delbtn updated

--- ecommerce/src/components/cart.component/cartItem.tsx
+++ ecommerce/src/components/cart.component/cartItem.tsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FcRating } from "react-icons/fc";
 import Button from "../../common/Button/Button.common";
-import { AiFillDelete } from "react-icons/ai";
+import { AiFillDelete, AiOutlineHeart, AiFillHeart } from "react-icons/ai";
+import { BsPlus } from "react-icons/bs";
+import { FiMinus } from "react-icons/fi";
+import { useSelector } from "react-redux";
 import "./_cartItem.scss";
 interface itemInterface {
   id: number;
@@ -22,14 +25,33 @@ interface cartItemProps {
 }
 
 const CartItem = (props: cartItemProps) => {
+  const { totalPrice } = useSelector((state: any) => state.products);
+  const [price, setTotalPrice] = useState(totalPrice);
+  const [count, setCount] = useState<number>(1);
   const { item, deleteCartItem, id } = props;
-
   // console.log(item);
+
+  const increaseCount = () => {
+    setCount(count + 1);
+  };
+  const decreaceCount = () => {
+    if (count === 0) {
+      setCount(count);
+    } else {
+      setCount(count - 1);
+    }
+  };
+  
+
+  useEffect(() => {
+    setTotalPrice(item[0].price);
+  }, []);
 
   return (
     <>
       <div className="cartItem-container">
         <div className="cart-img">
+          
           <img src={item[0].thumbnail} alt={item[0].title} />
 
           <button className="btn-del" onClick={() => deleteCartItem(id)}>
@@ -40,11 +62,23 @@ const CartItem = (props: cartItemProps) => {
           <h3>{item[0].title}</h3>
           <h4>{item[0].price} $</h4>
         </div>
-        <div className="cart-reating">
+        <div className="cart-rating">
           <span>
             <FcRating className="ratingIcon" />
           </span>
-          <span> {item[0].rating}</span>
+          <span> {item[0].description}</span>
+        </div>
+        <div className="order">
+          <button onClick={increaseCount}>
+            <BsPlus />
+          </button>
+          <span>{count}</span>
+          <button onClick={decreaceCount}>
+            <FiMinus />
+          </button>
+        </div>
+        <div className="total-price">
+          <span> Total Price : {price * count} $</span>
         </div>
         <div className="btn-section">
           <Button title="Checkout" />
