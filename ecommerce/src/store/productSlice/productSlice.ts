b63bb0b33d5631e92fd58e5387a8724f74206d6a
Tikaram Acharya <tikaramac19@gmail.com>
2022-12-30 14:53:23 +0545

    addtocart n filtercart logic

--- ecommerce/src/store/productSlice/productSlice.ts
+++ ecommerce/src/store/productSlice/productSlice.ts
@@ -1,9 +1,31 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
+interface itemInterface {
+  id: number;
+  title: string;
+  description: string;
+  brand: string;
+  category: string;
+  image: string[];
+  price?: number;
+  rating: number;
+  thumbnail: string;
+}
+interface intialStateType {
+  products: itemInterface[];
+  favroute: itemInterface[];
+  cartItems: itemInterface[];
+  deleteItem: itemInterface[];
+  pageTitle: string;
+}
+
 const initialState = {
   products: [],
-  pageTitle : "Products"
-};
+  favroute: [],
+  cartItems: [],
+  deleteItem: [],
+  pageTitle: "Products",
+} as intialStateType;
 
 const productSlice = createSlice({
   name: "products",
@@ -12,8 +34,23 @@ const productSlice = createSlice({
     addProducts: (state, actions) => {
       state.products = actions.payload;
     },
+    favrouteList: (state, actions) => {
+      let tempId = actions.payload;
+    },
+    addToCart: (state: any, actions) => {
+      // let getItem = actions.payload;
+      // console.log(getItem);
+      let tempCarts = [...state.cartItems];
+      tempCarts.push(actions.payload);
+
+      state.cartItems = tempCarts;
+      // console.log(tempCarts);
+    },
+    deleteCart: (state: any, actions) => {
+      state.cartItems = actions.payload;
+    },
   },
   extraReducers: (builder) => {},
 });
-export const { addProducts } = productSlice.actions;
+export const { addProducts, addToCart, deleteCart } = productSlice.actions;
 export default productSlice.reducer;
