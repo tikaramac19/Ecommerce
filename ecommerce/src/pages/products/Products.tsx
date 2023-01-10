9940d64c1259110e3a15a6f8f709fac0654c1ce0
Tikaram Acharya <tikaramac19@gmail.com>
2023-01-10 16:49:03 +0545

    .

--- ecommerce/src/pages/products/Products.tsx
+++ ecommerce/src/pages/products/Products.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { addProducts } from "../../store/productSlice/productSlice";
 import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
@@ -6,6 +6,7 @@ import "./_products.scss";
 import ProductsList from "../../components/products.component/productsList";
 import Layout from "../../components/layout/hoc/layout";
 import MiniNav from "../../common/miniNav/MiniNav";
+import { AiFillFastBackward, AiFillFastForward } from 'react-icons/ai'
 const ProductsPage = () => {
   // const { pageTitle } = useSelector((state: any) => state.productsSlice);
   // const product = useSelector((state: any) => state.productSlice);
@@ -13,17 +14,24 @@ const ProductsPage = () => {
   // console.log(product ,"fg;kdfjg;kf");
 
   // console.log(pageTitle)
+  let limit = 10;
+  const [page, setPage] = useState<number>(0)
+  const [total, setTotal] = useState<number>(1)
   const dispatch = useDispatch();
-  const fetchData = async () => {
-    const response = await axios.get("https://dummyjson.com/products");
+
+  const fetchData = useCallback(async () => {
+    const response = await axios.get(`https://dummyjson.com/products?limit=${limit}&skip=${page * 10}`);
     const result = await response.data;
+    // console.log('response: ', result)
+    setTotal(result.total)
     dispatch(addProducts(result.products));
-  };
+  }, [page]);
+
   // console.log(products);
   useEffect(() => {
-    // console.log("kera")
+    // console.log(userToken);
     fetchData();
-  }, []);
+  }, [fetchData]);
 
   return (
     <>
@@ -34,6 +42,20 @@ const ProductsPage = () => {
         </div>
         <div className="products-list">
           <ProductsList />
+        </div>
+        <div className="pagination-container">
+          <div className="backward-icon" onClick={() => setPage((prev) => prev === 0 ? prev : prev - 1)}>
+            <AiFillFastBackward />
+          </div>
+          <div className="num-pages">
+            {Array(Math.ceil(total / limit)).fill('').map((_, index) => {
+              return <span key={index} onClick={() => setPage(index)} ><button className={page === index ? 'span-active' : ''}>{index + 1}</button></span>
+            })}
+
+          </div>
+          <div className="forward-icon" onClick={() => setPage((prev) => prev === Math.ceil(total / limit) ? prev : prev + 1)}>
+            <AiFillFastForward />
+          </div>
         </div>
       </div>
     </>
