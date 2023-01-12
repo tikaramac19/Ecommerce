ad3d6efe57692f1aba3c98bcbe3f78599f6d7476
Tikaram Acharya <tikaramac19@gmail.com>
2023-01-12 11:27:20 +0545

    image slider completed

---
+++ ecommerce/src/components/imageSlider/imageSlider.component.tsx
@@ -0,0 +1,70 @@
+import React, { useState, useEffect } from "react";
+import "./_imageSlider.scss"
+import { FcPrevious, FcNext } from "react-icons/fc";
+import { VscCircleFilled } from 'react-icons/vsc';
+interface imagesType {
+    url: string,
+    title: string
+}
+
+interface imageSliderProps {
+    imageSlides: imagesType[]
+}
+
+const ImageSlider = (props: imageSliderProps) => {
+    const [currIndex, setCurrIndex] = useState<number>(0)
+    const { imageSlides } = props
+    const imgContiStyles = {
+        "backgroundImage": `url(${imageSlides[currIndex].url})`,
+        "height": "400px",
+        "width": "100%",
+        "backgroundSize": "contain",
+        "backgroundPosition": "center",
+        "positon": "relative",
+    }
+
+    const handlePrevious = () => {
+
+        const isFirstSlide = currIndex === 0;
+        const newIndex = isFirstSlide ? imageSlides.length - 1 : currIndex - 1
+        setCurrIndex(newIndex)
+    }
+    const handleNext = () => {
+        const isLastSlide = currIndex === imageSlides.length - 1;
+        const newIndex = isLastSlide ? 0 : currIndex + 1;
+        setCurrIndex(newIndex);
+    }
+    const hancleClickCircle = (tempId: number) => {
+        setCurrIndex(tempId);
+    }
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            // console.log('This will run every 4 second!');
+            const isLastSlide = currIndex === imageSlides.length - 1;
+            const newIndex = isLastSlide ? 0 : currIndex + 1;
+            setCurrIndex(newIndex);
+        }, 5000);
+        return () => clearInterval(interval);
+    }, [currIndex]);
+
+    return (<>
+        <div className="slider-container">
+            <div className="images-container" style={imgContiStyles}>
+                <FcPrevious className="previousIcon" onClick={handlePrevious} />
+                <FcNext className="nextIcon" onClick={handleNext} />
+
+                <div className="filled-circle-cont">
+                    {
+                        imageSlides.map((item: any, id: number) => {
+                            return <VscCircleFilled key={id.toString()} className={currIndex === id ? 'vsc-circle-active vsc-circle' : 'vsc-circle'} onClick={() => hancleClickCircle(id)} />
+                        })
+                    }
+                </div>
+
+            </div>
+        </div>
+    </>)
+}
+
+export default ImageSlider;
