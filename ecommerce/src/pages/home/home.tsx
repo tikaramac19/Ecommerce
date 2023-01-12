aa69736ad4254bc7d0d2d55bf5474e15a0283986
Tikaram Acharya <tikaramac19@gmail.com>
2023-01-12 11:27:44 +0545

    importing imageSLider comp

--- ecommerce/src/pages/home/home.tsx
+++ ecommerce/src/pages/home/home.tsx
@@ -1,11 +1,22 @@
 import React from "react";
 import Layout from "../../components/layout/hoc/layout";
 import MiniNav from "../../common/miniNav/MiniNav";
+import ImageSlider from "../../components/imageSlider/imageSlider.component";
 const HomePage = () => {
+
+  const imageSlides = [
+    { url: 'https://images.pexels.com/photos/4841273/pexels-photo-4841273.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', title: 'beutiful girl' },
+    { url: 'https://images.pexels.com/photos/5650050/pexels-photo-5650050.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', title: 'beutiful girl' },
+    { url: 'https://images.pexels.com/photos/7195060/pexels-photo-7195060.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', title: 'beutiful girl' },
+    { url: 'https://images.pexels.com/photos/7817699/pexels-photo-7817699.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', title: 'beutiful girl' },
+
+  ]
+
   return (
     <>
       <div className="home-container">
         <MiniNav />
+        <ImageSlider imageSlides={imageSlides} />
       </div>
     </>
   );
